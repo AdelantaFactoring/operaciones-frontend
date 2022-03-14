8c10ab4cab631b077dc035eb8dabac3e17544568
maiquin.ccaranpa <maiquinxs@gmail.com>
2022-03-14 18:26:56 -0500

    *

--- src/app/main/comercial/solicitudes/solicitudes-form/solicitudes-form.service.ts
+++ src/app/main/comercial/solicitudes/solicitudes-form/solicitudes-form.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {RequestMethod} from "../../../../shared/helpers/request-method";
 import {environment} from "environments/environment";
-import {SOLICITUD, CLIENTEPAGADOR} from "../../../../shared/helpers/url/comercial";
+import {SOLICITUD, CLIENTE} from "../../../../shared/helpers/url/comercial";
 import {Observable} from "rxjs";
 import {CONTENT_TYPE} from "../../../../shared/helpers/headers";
 
@@ -18,7 +18,7 @@ export class SolicitudesFormService {
 
   listarCliente(payload): Observable<any> {
     return this.requestMethod.get(
-      `${environment.apiUrl}${CLIENTEPAGADOR.listar}`,
+      `${environment.apiUrl}${CLIENTE.listar}`,
       `?idTipo=${payload.idTipo}&search=${payload.search}&pageIndex=${payload.pageIndex}&pageSize=${payload.pageSize}`,
       {
         'Content-Type': CONTENT_TYPE.json
