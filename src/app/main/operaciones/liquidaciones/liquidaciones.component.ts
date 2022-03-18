37f5ca18842c71e58106bf376266db2e362551ac
Mark Jorge <mjorge@createksystem.com>
2022-03-18 14:00:25 -0500

    >>

--- src/app/main/operaciones/liquidaciones/liquidaciones.component.ts
+++ src/app/main/operaciones/liquidaciones/liquidaciones.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UtilsService} from "../../../shared/services/utils.service";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {SolicitudCab} from "../../../shared/models/comercial/solicitudCab";
+import {SolicitudesService} from "../../comercial/solicitudes/solicitudes.service";
 
 @Component({
   selector: 'app-liquidaciones',
@@ -7,7 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LiquidacionesComponent implements OnInit {
   public contentHeader: object;
-  constructor() {
+  public seleccionarTodoSolicitud: boolean = false;
+  public cambiarIconoSolicitud: boolean = false;
+  public solicitudes: SolicitudCab[] = [];
+
+  public searchSolicitud: string = '';
+  public collectionSizeSolicitud: number = 0;
+  public pageSizeSolicitud: number = 10;
+  public pageSolicitud: number = 1;
+
+  constructor(
+    private modalService: NgbModal,
+    private utilsService: UtilsService,
+    private solicitudesService: SolicitudesService) {
     this.contentHeader = {
       headerTitle: 'Liquidaciones',
       actionButton: true,
@@ -35,4 +51,65 @@ export class LiquidacionesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onListarSolicitudes(): void {
+    this.utilsService.blockUIStart('Obteniendo información...');
+    this.solicitudesService.listar({
+      idConsulta: 5,
+      search: this.searchSolicitud,
+      pageIndex: this.pageSolicitud,
+      pageSize: this.pageSizeSolicitud
+    }).subscribe((response: SolicitudCab[]) => {
+      this.solicitudes = response;
+      this.collectionSizeSolicitud = response.length > 0 ? response[0].totalRows : 0;
+
+      this.utilsService.blockUIStop();
+    }, error => {
+      this.utilsService.blockUIStop();
+      this.utilsService.showNotification('An internal error has occurred', 'Error', 3);
+    });
+  }
+
+  onCambiarVisibilidadDetalleTodoSolicitud(): void {
+    this.cambiarIconoSolicitud = !this.cambiarIconoSolicitud;
+    this.solicitudes.forEach(el => {
+      el.cambiarIcono = this.cambiarIconoSolicitud;
+      document.getElementById('tr' + el.idSolicitudCab).style.visibility = (el.cambiarIcono) ? "visible" : "collapse";
+      document.getElementById('detail' + el.idSolicitudCab).style.display = (el.cambiarIcono) ? "block" : "none";
+    });
+  }
+
+  onCambiarVisibilidadDetalleSolicitud(item: any): void {
+    item.cambiarIcono = !item.cambiarIcono;
+    document.getElementById('tr' + item.idSolicitudCab).style.visibility = (item.cambiarIcono) ? "visible" : "collapse";
+    document.getElementById('detail' + item.idSolicitudCab).style.display = (item.cambiarIcono) ? "block" : "none";
+  }
+
+  onSeleccionarTodoSolicitud(): void {
+    this.solicitudes.forEach(el => {
+      if (el.checkList)
+        el.seleccionado = this.seleccionarTodoSolicitud;
+    });
+  }
+
+  onRefrescarSolicitud() {
+    this.onListarSolicitudes();
+  }
+
+  onGenerarLiquidaciones(modal): void {
+    this.searchSolicitud = '';
+    this.onListarSolicitudes();
+    setTimeout(() => {
+      this.modalService.open(modal, {
+        scrollable: true,
+        backdrop: 'static',
+        windowClass: 'my-class',
+        animation: true,
+        //size: 'lg',
+
+        beforeDismiss: () => {
+          return true;
+        }
+      });
+    }, 0);
+  }
 }
